c4783c73988d0ed8cbedf820621809413166162a
akachi11 <ogogorchimadika6@gmail.com>
2026-09-13 14:57:39 +0100

    create get and post matches endpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import {matchRouter} from "./routes/matches.js";
 
 const app = express();
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Hello from express"})
+})
+
+app.use("/matches", matchRouter)
 
 app.listen(8000, () => console.log("Server running on port 8000"))
